39a48bb338309afd9942b2bc05c106c7520b44e3
Maksim <liakhovetsmaksim@gmail.com>
2017-12-10 00:59:25 +0300

    adding sql ins

---
+++ src/main/java/guru/springframework/Repository/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.Repository;
+
+import guru.springframework.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe,Long>{
+}
