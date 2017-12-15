fb69922a82156385bbe9e3b13588cb85bdb1314f
Maksim <maksim.lyahovets@service-it.by>
2017-12-15 13:29:39 +0300

    add view ingridient

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -5,4 +5,6 @@ import guru.springframework.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand find1ByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
