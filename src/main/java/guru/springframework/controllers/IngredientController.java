912e8e243af0749049a1aa792175f45ea2aa66b3
Maksim <maksim.lyahovets@service-it.by>
2017-12-15 11:11:07 +0300

    add view ingredient

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.IngredientService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,16 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IngredientController {
 
-    private  final RecipeService recipeService;
+    private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
-    @GetMapping
+
     @RequestMapping("recipe/{recipeId}/ingredients")
-    public String listIngredients(@PathVariable String recipeId, Model model){
+    public String listIngredients(@PathVariable String recipeId,Model model ){
         log.debug("Getting ingredient List" + recipeId);
         model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(recipeId)));
         return "recipe/ingredient/list";
+    }
+
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public  String showById(@PathVariable String recipeId,
+                            @PathVariable String id ,Model model){
+            model.addAttribute("ingredient",ingredientService.find1ByRecipeIdAndIngredientId(Long.valueOf(recipeId),Long.valueOf(id)));
+            return "recipe/ingredient/show";
     }
 }
--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -1,4 +1,8 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.IngredientCommand;
+
 public interface IngredientService {
+
+    IngredientCommand find1ByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 }
